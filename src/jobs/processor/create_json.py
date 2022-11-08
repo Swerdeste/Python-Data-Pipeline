565cdc89300682b95b7c04e3657a8f841edfe0c5
Arthur Chane-Sam <achanesam@me.com>
2022-11-09 00:19:38 +0100

    Adapt json output creation function

--- src/jobs/processor/create_json.py
+++ src/jobs/processor/create_json.py
@@ -1,37 +1,38 @@
 import pandas as pd
 from genpipes import declare
 import json
+from typing import Iterator
 
 
 @declare.processor()
-def create_json_output(df: pd.DataFrame, output_name: str):
+def create_json(stream: Iterator[pd.DataFrame], output_name: str):
     """
 
-    :param df:
+    :param stream:
     :param output_name:
     :return:
     """
+    for df in stream:
+        # json doesn't like datetime object so we transform it to into a readable object
 
-    # json doesn't like datetime object so we transform it to into a readable object
+        df['date'] = df['date'].dt.strftime('%Y-%d-%m')
 
-    df['date'] = df['date'].dt.strftime('%Y-%d-%m')
+        # create Dictionnary
 
-    # create Dictionnary
+        dic = df.groupby(['attcode', 'drug']).apply(lambda x: x[['date', 'id', 'title', 'type', 'journal']]
+                                                    .dropna()
+                                                    .to_dict('records')) \
+            .reset_index() \
+            .rename(columns={0: 'mentions'}) \
+            .to_json(orient='records')
 
-    dic = df.groupby(['attcode', 'drug']).apply(lambda x: x[['date', 'id', 'title', 'type', 'journal']]
-                                                .dropna()
-                                                .to_dict('records')) \
-        .reset_index() \
-        .rename(columns={0: 'mentions'}) \
-        .to_json(orient='records')
+        with open(output_name, 'w', encoding='utf8') as output:
+            json.dump(
+                json.loads(dic),
+                output,
+                indent=2,
+                sort_keys=True,
+                ensure_ascii=False
+            )
 
-    with open(output_name, 'w', encoding='utf8') as output:
-        json.dump(
-            json.loads(dic),
-            output,
-            indent=2,
-            sort_keys=True,
-            ensure_ascii=False
-        )
-
-    return df
+        yield df
