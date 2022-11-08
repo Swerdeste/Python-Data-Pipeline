6f995abd03f67114ae2c4d402d876d2d09741f5d
Arthur Chane-Sam <achanesam@me.com>
2022-11-09 00:15:50 +0100

    Add pipeline builder

--- src/utils/pipeline_builder.py
+++ src/utils/pipeline_builder.py
@@ -1,13 +1,38 @@
 from genpipes import compose
+import yaml
+import importlib
+import inspect
 
 
 class PipelineBuilder:
 
-    def __init__(self, config):
-        self.config = config
+    def __init__(self):
+        self.pipe = None
+        self.steps = []
+    def build(self, config_path):
+        with open(config_path) as f:
+            steps = yaml.safe_load(f)
+        for step in steps:
+            step_name = step['step-name']
+            del step['step-name']
+            step_path = step['filepath']
+            del step['filepath']
+            step_job = getattr(importlib.import_module(step_path), step_name)
+            step_description = inspect.getdoc(step_job)
 
-    def build(self):
-        return compose.Pipeline(self.config)
+            step_config = step
+
+            self.steps.append(
+                (
+                    step_description,
+                    step_job,
+                    step_config
+                )
+            )
+        self.pipe = compose.Pipeline(steps=self.steps)
+
+        return compose.Pipeline(step)
 
     def run(self):
-        return self.build().run()
+        data = self.pipe.run()
+        return data
